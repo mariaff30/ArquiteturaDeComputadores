78e72281abeb401c71536b9572cd67cd3e4532ec
mariaff30 <58263269+mariaff30@users.noreply.github.com>
2019-11-27 23:18:32 -0300

    Add files via upload

---
+++ divisao.s
@@ -0,0 +1,38 @@
+.data
+
+numero: .asciiz "Digite o numero: "
+resultado: .asciiz "Divisao entre os numeros: "
+.text
+
+main:
+
+li $v0,4
+la $a0, numero
+syscall
+
+li $v0,5 
+syscall
+
+move $t0, $v0
+
+li $v0,4
+la $a0, numero
+syscall
+
+li $v0,5
+syscall
+
+move $t1, $v0
+
+sub $t2, $t1, $t0
+
+
+li $v0,4
+la $a0, resultado
+syscall
+
+li $v0,1
+move $a0, $t2
+syscall
+
+jr $ra
